add9e6fbd1406cd44f564dc6b3b8c715a8063d97
Axel918 <drakeaxel.mayo@benilde.edu.ph>
2021-10-05 08:42:48 +0800

    Updated version of Quiz 2

--- Quiz_2/src/com/drakemayo/Main.java
+++ Quiz_2/src/com/drakemayo/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Linked List");
         Player asuna = new Player(1, "Asuna", 100);
         Player lethalbacon = new Player(2, "LethalBacon", 205);
         Player hpDeskjet = new Player(3, "HPDeskjet", 34);
